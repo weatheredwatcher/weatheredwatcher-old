36aa0f1a76070b7c1b0c7f305df3f1c342e590b0
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-22 12:26:36 -0500

    added blog model! oops!

---
+++ application/models/blog_model.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Blog extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+		
+	function function_name()
+	{
+		
+	}
+}
