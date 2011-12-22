1698aa304e6a34d9937a172eddfdb6a51b90747f
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-22 13:06:38 -0500

    added projects and portfolio copy

--- application/views/site_projects.php
+++ application/views/site_projects.php
@@ -7,7 +7,16 @@
 
         <!-- main -->
            <div id="main">
-<%= yield %>
+			<h3>Projects and Portfolio</h3>
+			<p>
+			<span class="project">This site</span> is really one of my little projects.  This is my new and improved site.  It's hand coded in Rails 3
+			and although it is currently not functioning, it will contain my blog shortly.  I was using posterous for my blog, and my site just pulled in a RSS stream.  Now I am in the process of migrating the entries from posterous to my new system here.  It will give a bit more streamlined and together feel.
+			</p>
+			<h3>Sow</h3><p> Sow is an open-source script that I wrote in Ruby to create a project structure for html or php based projects.  It creates the project folder, folders for images css, and javascript.
+			It also creates a XHTML Strict index.html page, as well as download three javascript libraries.  It is easy to customize for anyones needs.  You can get the code from the project page on GitHub <a href="http://github.com/weatheredwatcher/Sow">http://github.com/weatheredwatcher/Sow</a>
+
+			It is released under the GPL.
+			</p>
 
         <!-- /main -->
                 </div>
