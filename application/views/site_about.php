7d0c89144263cd5c878757b45d53be4ef3235123
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-22 13:08:04 -0500

    changed headers and realigned text in editor

--- application/views/site_about.php
+++ application/views/site_about.php
@@ -4,20 +4,30 @@
 
         <!-- content -->
    <div id="content">
-<h2>About Me</h2>
+<h3>About Me</h3>
         <!-- main -->
            <div id="main">
-			<p>I was born in Greensboro NC. When I was 12, my parents moved the entire family to Kazakhstan whereI became very active in the hacker community. Me and my friend Kostya Tayban started running BBS's from our homes and were for a time connected to the FidoNet.</p>
+			<p>I was born in Greensboro NC. When I was 12, my parents moved the entire family to Kazakhstan 
+				whereI became very active in the hacker community. Me and my friend Kostya Tayban started 
+				running BBS's from our homes and were for a time connected to the FidoNet.</p>
 
-			<p>My wife and I came to the states in 2000 and in 2001 moved to Charlotte NC. In Charlotte I did IT work for several Car Dealerships as well as some web and software development for a few small start-up ventures.</p>
+			<p>My wife and I came to the states in 2000 and in 2001 moved to Charlotte NC. In Charlotte 
+				I did IT work for several Car Dealerships as well as some web and software development 
+				for a few small start-up ventures.</p>
 
-			<p>In 2006, moved the family to Columbia SC were I begain working for the Institute of Strategic Languages and Cultures as a web-developer.</p>
+			<p>In 2006, moved the family to Columbia SC were I begain working for the Institute of 
+				Strategic Languages and Cultures as a web-developer.</p>
 
-			<p>I work in PHP, Ruby on Rails, Ajax/JavaScript, Python and Clojure. I develop entirely on a mac with TextMate.</p>
+			<p>I work in PHP, Ruby on Rails, Ajax/JavaScript, Python and Clojure. I develop entirely 
+				on a mac with TextMate.</p>
 
-			<p>Over the years, I have worked for several start ups, including Lead Programmer for Leveraged Media, a Tech Company disguised as a Marketing Company disguised as a Tech company and most recently for Political Exchange, a Political Social Media Outlet.</p>
+			<p>Over the years, I have worked for several start ups, including Lead Programmer for 
+				Leveraged Media, a Tech Company disguised as a Marketing Company disguised as a 
+				Tech company and most recently for Political Exchange, a Political Social Media Outlet.</p>
 
-			<p>I am currently freelancing as a Consultant here in Columbia, SC. I focus on building software infrastructures for start-ups, using FOSS in Health Care and teaching and training on FOSS.</p>
+			<p>I am currently freelancing as a Consultant here in Columbia, SC. I focus on building 
+				software infrastructures for start-ups, using FOSS in Health Care and teaching and 
+				training on FOSS.</p>
 
 			<p>I also do some dabbling in social media integration.</p>
 
