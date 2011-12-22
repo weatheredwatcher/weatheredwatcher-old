69c95ba7dabe739a933e5d218f0d2630d8242798
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-22 12:28:42 -0500

    loaded the model

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -9,6 +9,7 @@ class Blog extends CI_Controller {
 		
 	function index()
 	{
+		$this->load->model('Blog');
 		$query = $this->db->query("SELECT * FROM blog WHERE sort desc");
 		$data['entries'] = $query->result();
 	  $this->load->view('blog/blog_main', $data);	
