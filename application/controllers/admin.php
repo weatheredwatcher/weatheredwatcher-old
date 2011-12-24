0ff162f71b63273b7b4ab9cbcd4c7e18c72c699a
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-23 20:14:13 -0500

    added admin controller and created some methods

---
+++ application/controllers/admin.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+		
+	function index()
+	{
+	//todo will need to add authentication code here.
+		
+	}
+	
+	function add_blog_entry()
+	{
+		//todo need to add code to add blog entry here
+	}
+	
+	function add_dynamic_page()
+	{
+		
+		//code for creating dynamic pages, whose content are located in the database as opposed to the view
+		
+	}
+}
