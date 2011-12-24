f675b3bacd828de6286d6a18b2e6f3bd6c07f6b5
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-23 19:20:55 -0500

    added an alias to my blog model

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -9,7 +9,7 @@ class Blog extends CI_Controller {
 		
 	function index()
 	{
-		$this->load->model('Blog_model');
+		$this->load->model('Blog_model', 'blog');
 		//$query = $this->db->query("SELECT * FROM blog ORDER BY timestamp DESC");
 		$data['entries'] = $this->Blog->get_last_ten_entries();
 		 //$query->result();
