5f9a6053b99f817d37f26455668671e43df3489f
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-23 19:17:43 -0500

    filled out the blog model, added some methods

--- application/models/blog_model.php
+++ application/models/blog_model.php
@@ -7,8 +7,28 @@ class Blog_model extends CI_Model {
 		parent::__construct();
 	}
 		
-	function function_name()
-	{
-		
-	}
+	function get_last_ten_entries()
+	    {
+	        $query = $this->db->get('entries', 10);
+	        return $query->result();
+	    }
+
+	    function insert_entry()
+	    {
+	        $this->title   = $_POST['title']; // please read the below note
+	        $this->entry = $_POST['entry'];
+	        $this->timestamp   = time();
+
+	        $this->db->insert('entries', $this);
+	    }
+
+	    function update_entry()
+	    {
+	        $this->title   = $_POST['title'];
+	        $this->entry = $_POST['entry'];
+	        $this->timestamp    = time();
+
+	        $this->db->update('entries', $this, array('id' => $_POST['id']));
+	    }
+	
 }
