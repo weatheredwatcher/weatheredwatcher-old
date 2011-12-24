f47750d58666a8b1caa6ca46f9b187f24562cde4
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-23 22:25:55 -0500

    fleshed out the controller layout based on the admin template

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -13,25 +13,37 @@ class Admin extends CI_Controller {
 		
 	}
 	
-	function add_blog_entry()
+	function write()
 	{
-		//todo need to add code to add blog entry here
+		//todo need to add code to add blog entry/pages here
 	}
 	
-	function add_dynamic_page()
+	function manage()
 	{
 		
-		//code for creating dynamic pages, whose content are located in the database as opposed to the view
+		//code to manage the blog/pages
 		
 	}
+	
+	function settings()
+	{
+		
+		//custom page settings
+		
+	}
+	
+	function users()
+	{
+		
+		//adding users form 
+		
+	}
+	
 	
 	function auth()
 	{
+		//TODO this is where we will authorize the access to the admin page.  This is going to create a pop up that will be utillized to authenticate the user.  We will use sessions data to do this
 		
-		///*
-			TODO this is where we will authorize the access to the admin page.  This is going to create a pop
-			that will be utillized to authenticate the user.  We will use sessions data to do this
-		*/
 		
 	}
 }
