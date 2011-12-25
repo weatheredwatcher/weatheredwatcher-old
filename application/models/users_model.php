7468b928636cfee908e602c45e05bff4c7949cb5
weatheredwatcher <weatheredwatcher@gmail.com>
2011-12-25 14:58:35 -0500

    took the user auth method out again

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -23,30 +23,5 @@ class Users_model extends CI_Model {
 		
 	}
 	
-	function auth()
-	{
-			$this->login = $this->input->post('login');
-			$this->password = $this->input->post('password');
-			$query = $this->db->get_where('users', array('username' => $this->login));
-			foreach ($query->result() as $row)
-			{
-			    if ($row->password == $this->password) {
-				//this is where we set the session data to logged in
-				$loguser = array(
-					               'id'        => $row->id,
-				                   'username'  => $this->login,
-								   'name'      => $row->name,
-				                   'email'     => $row->email,
-				                   'logged_in' => TRUE
-				               );
-
-				$this->session->set_userdata($loguser);
-				/*
-					TODO save session data, ip and date in the profile model.
-				*/
-				}else {
-					   //this is where we redirect back to the login page
-					redirect('admin/', location);
-					  }
-	}
+	
 }
