dbe85d079d1d25fade444ca313bd4365593f9135
weatheredwatcher <weatheredwatcher@gmail.com>
2012-01-05 00:25:51 -0500

    soso

--- application/views/site_main.php
+++ application/views/site_main.php
@@ -8,10 +8,10 @@
         <!-- main -->
            <div id="main">
 			   <div id="scrapbook">
-			   <?=img('scrapbook1.jpg');?>
-			   <?=img('scrapbook2.jpg');?>
-			   <?=img('scrapbook3.jpg');?>
-			   <?=img('scrapbook4.jpg');?>
+			   <?=img('images/scrapbook1.jpg');?>
+			   <?=img('images/scrapbook2.jpg');?>
+			   <?=img('images/scrapbook3.jpg');?>
+			   <?=img('images/scrapbook4.jpg');?>
 			   </div>
 			  <p>
 Hey!  My name is David Duggins and this is my site!  I am a php developer living currently in Columbia SC.  I have the greatest two jobs in the world!  One, I am a freelance developer working for myself and two I am the father of an awesome little red-headed geek-girl or 6!  Take some time to look around!</p>
