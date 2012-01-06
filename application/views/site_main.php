d1082ceca6b4f7e0799cbfabe6dea27a92e26c76
weatheredwatcher <weatheredwatcher@gmail.com>
2012-01-06 18:53:49 -0500

    made a few text changes

--- application/views/site_main.php
+++ application/views/site_main.php
@@ -14,7 +14,7 @@
 			   <?=img('images/scrapbook4.jpg');?>
 			   </div>
 			  <p>
-Hey!  My name is David Duggins and this is my site!  I am a php developer living currently in Columbia SC.  I have the greatest two jobs in the world!  One, I am a freelance developer working for myself and two I am the father of an awesome little red-headed geek-girl or 6!  Take some time to look around!</p>
+Hey!  My name is David Duggins and this is my site!  I am a php developer living currently in Columbia SC.  I have the greatest two jobs in the world!  One, I am a freelance developer working for myself and two I am the father of an awesome little red-headed geek-girl of 6!  Take some time to look around!</p>
 
         <!-- /main -->
                 </div>
--- application/views/site_projects.php
+++ application/views/site_projects.php
@@ -9,15 +9,14 @@
            <div id="main">
 			<h3>Projects and Portfolio</h3>
 			<p>
-			<span class="project">This site</span> is really one of my little projects.  This is my new and improved site.  It's hand coded in Rails 3
-			and although it is currently not functioning, it will contain my blog shortly.  I was using posterous for my blog, and my site just pulled in a RSS stream.  Now I am in the process of migrating the entries from posterous to my new system here.  It will give a bit more streamlined and together feel.
+			<span class="project">This site</span> is really one of my little projects.  This is my new and improved site.  It's hand coded in Codeigniter and is also the testing grounds for my very own base cms in Codeigniter.
 			</p>
 			<h3>Sow</h3><p> Sow is an open-source script that I wrote in Ruby to create a project structure for html or php based projects.  It creates the project folder, folders for images css, and javascript.
 			It also creates a XHTML Strict index.html page, as well as download three javascript libraries.  It is easy to customize for anyones needs.  You can get the code from the project page on GitHub <a href="http://github.com/weatheredwatcher/Sow">http://github.com/weatheredwatcher/Sow</a>
 
 			It is released under the GPL.
 			</p>
-			<h3></h3><p>This site is also testing grounds for my very own base cms in Codeigniter.
+			
 
         <!-- /main -->
                 </div>
