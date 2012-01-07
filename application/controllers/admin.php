c9a99d8d929fc60a85b13422764e5d9b7d444c55
weatheredwatcher <weatheredwatcher@gmail.com>
2012-01-06 21:26:16 -0500

    put everything behind the secure wall

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -30,42 +30,93 @@ class Admin extends CI_Controller {
 	
 	function write()
 	{
-		$this->load->model('Users_model', 'users');
-		$data['authors'] = $this->users->get_authors();
-		$this->load->view('admin/write_view', $data);
+		if ($this->session->userdata('logged_in') == FALSE)
+		{
+		     $this->load->view('admin/index_view');
+		}
+		else
+		{
+			$this->load->model('Users_model', 'users');
+			$data['authors'] = $this->users->get_authors();
+			$this->load->view('admin/write_view', $data);
+			
+		}
+		
+		
 	}
 	
 	function insert_page()
 	{
 		
-		$this->load->model('Blog_model', 'blog');
-		$this->blog->insert_entry();
-		redirect('admin/manage', 'refresh');
+		if ($this->session->userdata('logged_in') == FALSE)
+		{
+		     $this->load->view('admin/index_view');
+		}
+		else
+		{
+			$this->load->model('Blog_model', 'blog');
+			$this->blog->insert_entry();
+			redirect('admin/manage', 'refresh');
 	
+			
+		}
+		
+		
 		
 	}
 	
 	function manage()
 	{
 		
-		$this->load->model('Blog_model', 'blog');
-		$data['entries'] = $this->blog->get_last_ten_entries();
-		$this->load->view('admin/manage', $data);
+		if ($this->session->userdata('logged_in') == FALSE)
+		{
+		     $this->load->view('admin/index_view');
+		}
+		else
+		{
+			$this->load->model('Blog_model', 'blog');
+			$data['entries'] = $this->blog->get_last_ten_entries();
+			$this->load->view('admin/manage', $data);
+			
+			
+		}
+		
+		
 		
 	}
 	
 	function settings()
 	{
 		
-		$this->load->view('admin/settings_view');
+		if ($this->session->userdata('logged_in') == FALSE)
+		{
+		     $this->load->view('admin/index_view');
+		}
+		else
+		{
+			$this->load->view('admin/settings_view');
+		}
+		
+		
 		
 	}
 	
 	function users()
 	{
-		$this->load->model('Users_model', 'users');
-		$data['users'] = $this->users->get_users();
-		$this->load->view('admin/users_view', $data);
+		
+		if ($this->session->userdata('logged_in') == FALSE)
+		{
+		     $this->load->view('admin/index_view');
+		}
+		else
+		{
+			$this->load->model('Users_model', 'users');
+			$data['users'] = $this->users->get_users();
+			$this->load->view('admin/users_view', $data);
+			
+		}
+		
+		
 		
 	}
 
