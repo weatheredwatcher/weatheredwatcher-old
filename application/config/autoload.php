3326e395e00686f5715a86f14f058b45ed13b89f
weatheredwatcher <weatheredwatcher@gmail.com>
2012-01-06 21:28:56 -0500

    added the form helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['libraries'] = array('database', 'session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'html');
+$autoload['helper'] = array('url', 'html', 'form');
 
 
 /*
