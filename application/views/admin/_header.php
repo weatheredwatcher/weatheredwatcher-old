5075856cf51aa55a85a2c1d932412f32d8dc7988
weatheredwatcher <weatheredwatcher@gmail.com>
2012-01-13 11:29:48 -0500

    again

--- application/views/admin/_header.php
+++ application/views/admin/_header.php
@@ -9,4 +9,4 @@
 	<div class="wrapper">
 	
 		<h1 class="logo">WeatheredWatcher Admin</h1>
-		<p class="txt_right">Logged in as <strong><Ninja Admin</strong>  <span class="v_line"> | </span> <a href="admin/logout"> Logout</a></p>
+		<p class="txt_right">Logged in as <strong><?=$username?></strong>  <span class="v_line"> | </span> <a href="admin/logout"> Logout</a></p>
