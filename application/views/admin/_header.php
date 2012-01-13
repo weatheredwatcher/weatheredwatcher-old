27d21c7b3f6ea6faeadf372f03a47e9e0e2fc2a6
weatheredwatcher <weatheredwatcher@gmail.com>
2012-01-13 11:23:20 -0500

    a few header mods

--- application/views/admin/_header.php
+++ application/views/admin/_header.php
@@ -9,4 +9,4 @@
 	<div class="wrapper">
 	
 		<h1 class="logo">WeatheredWatcher Admin</h1>
-		<p class="txt_right">Logged in as <strong><?php if(isset($this->session->userdata('username'))): echo $this->session->userdata('username'); else: echo 'Login'; endif; ?> </strong>  <span class="v_line"> | </span> <a href="#"> Logout</a></p>
+		<p class="txt_right">Logged in as <strong><?=$username;?></strong>  <span class="v_line"> | </span> <a href="/admin/logout"> Logout</a></p>
