2a634ea2618ba1c96230eb0242682f0f131b366f
David Duggins <weatheredwatcher@gmail.com>
2012-02-08 01:56:46 -0500

    contact form

--- application/views/contact_form.php
+++ application/views/contact_form.php
@@ -1,6 +1,18 @@
 <?= $this->load->view('_header');?>
 
+<?php
 
+$from_data = array(
+              'name'        => 'from',
+              'maxlength'   => '100',
+              'size'        => '50'
+              );
+$message_data = array(
+              'name'        => 'message',
+              'cols'        => '50'
+              );
+
+?>
 <!-- content-outer -->
 <div id="content-wrap" class="clear" >
 
@@ -12,11 +24,11 @@
            
 <?=form_open('contact/send_email'); ?>
 <?=form_label('From:', 'from'); ?> 
-<?=form_input('from', ''); ?><br />
+<?=form_input($from_data); ?><br />
 <?=form_label('Subject:', 'subject'); ?> 
 <?=form_input('subject', ''); ?><br />
 <?=form_label('Message:', 'message'); ?>
-<?=form_textarea('message', '',array('cols' => '50')); ?> <br />
+<?=form_textarea($message_data); ?> <br />
 <?=form_submit('submit', 'Submit'); ?>
 <?=form_close(); ?>
 
