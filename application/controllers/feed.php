19b3160d1688a6ec777509e079b6b897b1b746e5
David Duggins <weatheredwatcher@gmail.com>
2012-03-07 10:24:24 -0500

    testing

--- application/controllers/feed.php
+++ application/controllers/feed.php
@@ -5,17 +5,19 @@ class Feed  extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		 $this->load->helper('xml');  
-		 $this->load->helper('text');  
-		 $this->load->model('Blog_model', 'blog');  
+
 	}
 		
 	function index()
 	{
-		$this->load->helper('inflector');
-	       
-		$data['entries'] = $this->blog->get_last_ten_entries();
-		$this->load->view('rss', $data);
+		//$this->load->helper('inflector');
+	    //$this->load->helper('xml');  
+		//$this->load->helper('text');  
+		//$this->load->model('Blog_model', 'blog');    
+		
+		//$data['entries'] = $this->blog->get_last_ten_entries();
+		$data['entries'] = 'Hello Sparky!;
+		$this->load->view('rss_test', $data);
 		//why is this not working yet?
 		
 		     
