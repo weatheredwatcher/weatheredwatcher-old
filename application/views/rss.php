ac12ad0e711eebe344f020e59d29f4de972dfd1a
David Duggins <weatheredwatcher@gmail.com>
2012-03-07 10:43:48 -0500

    testing

--- application/views/rss.php
+++ application/views/rss.php
@@ -1,4 +1,4 @@
-<?php echo ('<?xml version="1.0" encoding="utf-8" standalone="no"?>'. '<br />'); ?>
+<?php echo ('<?xml version="1.0" encoding="utf-8" standalone="no"?>'. \n); ?>
 <rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
     <channel>
         <title>....mindless rantings of a lunatic</title>
