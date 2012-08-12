394c15bba87c99e737f7ba328056a35a71c5de73
David Duggins <weatheredwatcher@gmail.com>
2012-08-11 22:29:59 -0400

    added speaker files

--- application/views/site_speaking.php
+++ application/views/site_speaking.php
@@ -16,6 +16,13 @@
 			<p> In 2011 I was a speaker at POSSCON here in Columbia SC.  I gave a talk on using Open Source Software to Run you business.  I also gave a workshop on using various Open Source Software programs.</p>
 
       <p> In 2012 I co-hosted a php "Birds of a Feather" luncheon with the fabulous Elizabeth Naramore. I discussed  the creation of a Columbia based PHU Users Group and demoed the new server feature in php 5.4.0  </p>
+
+<h3>North East PHP Conference</h3>
+<p> </p>
+<p>Downloads:
+  <a href="http://weatheredwatcher.com/downloads/NEPHP.tar.7z">Source Code</a><br />
+  <a href="http://weatheredwatcher.com/downloads/coding_a_basic_blog_with_codigniter.odp">Slides</a>
+</p>
 <h3>User Groups</h3>
 <p> I occasionally speak at various user groups on php, security, linux, and programming in general.</p>
         <!-- /main -->
