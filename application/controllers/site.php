28bbf8557973b61deff6b56c392ed346ee1448ce
David Duggins <weatheredwatcher@gmail.com>
2012-08-18 15:28:23 -0400

    curl stuff

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -22,7 +22,7 @@ $this->load->view('site_main');
 		
 		$ch = curl_init();		
 		curl_setopt($ch, CURLOPT_URL, "https://api.github.com/gists");
-		curl_setopt($ch, CURLOPT_USERPWD,  "[weatheredwatcher]:[m0rpheus]");
+		curl_setopt($ch, CURLOPT_USERPWD,  "weatheredwatcher:m0rpheus");
 		$data['gists'] = json_decode(curl_exec($ch));
 		// close cURL resource, and free up system resources
 		curl_close($ch);
