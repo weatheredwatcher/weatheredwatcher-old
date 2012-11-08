e0395a82b91c2e2d208e6cd42f57308413e6cdc5
David Duggins <weatheredwatcher@gmail.com>
2012-11-08 09:54:34 -0500

    changed the base url config to fix a bug with Orchestra

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+
+$config['base_url'] = $_SERVER['HTTP_X_ORCHESTRA_PROTO'] . "://" . $_SERVER['HTTP_HOST'] . "/";
 
 /*
 |--------------------------------------------------------------------------
